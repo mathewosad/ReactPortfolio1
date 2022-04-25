bef735beae8c3dbcefe5ae765acfda727936614d
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-04-24 20:39:48 -0700

    services components finished

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,96 @@
+// import React from 'react'
+// import './services.css'
+// import {VscCheckAll} from 'react-icons/vsc'
+
+// const Services = () => {
+//   return (
+//     <section id='services'>
+//       <h5>What I Offer</h5>
+//       <h2>Services</h2>
+
+//       <div className="container services__container">
+//                     {/* UX/UI Developer section  */}
+//         <article className='services'>
+//           <div className="service__head">
+//             <h3>UI/UX Developer</h3>
+//           </div>
+
+//           <ul className="service__list">
+//             <li>
+//               <VscCheckAll className='service__list-icon'/>
+//               <p>HTML,CSS</p>
+//             </li>
+//             <li>
+//               <VscCheckAll className='service__list-icon'/>
+//               <p>HTML,CSS</p>
+//             </li>
+//             <li>
+//               <VscCheckAll className='service__list-icon'/>
+//               <p>HTML,CSS</p>
+//             </li>
+//             <li>
+//               <VscCheckAll className='service__list-icon'/>
+//               <p>HTML,CSS</p>
+//             </li>
+//           </ul>
+//         </article>
+// {/* UX/UI Developer section */}
+
+// {/* Front-End Developer section */}
+// <article className='services'>
+//           <div className="service__head">
+//             <h3>Front-End Developer</h3>
+//           </div>
+
+//           <ul className="service__list">
+//             <li>
+//               <VscCheckAll className='service__list-icon'/>
+//               <p>HTML,CSS</p>
+//             </li>
+//             <li>
+//               <VscCheckAll className='service__list-icon'/>
+//               <p>HTML,CSS</p>
+//             </li>
+//             <li>
+//               <VscCheckAll className='service__list-icon'/>
+//               <p>HTML,CSS</p>
+//             </li>
+//             <li>
+//               <VscCheckAll className='service__list-icon'/>
+//               <p>HTML,CSS</p>
+//             </li>
+//           </ul>
+//         </article>
+
+// {/* Front-End Developer section */}
+// <article className='services'>
+//           <div className="service__head">
+//             <h3>Back-End Developer</h3>
+//           </div>
+
+//           <ul className="service__list">
+//             <li>
+//               <VscCheckAll className='service__list-icon'/>
+//               <p>HTML,CSS</p>
+//             </li>
+//             <li>
+//               <VscCheckAll className='service__list-icon'/>
+//               <p>HTML,CSS</p>
+//             </li>
+//             <li>
+//               <VscCheckAll className='service__list-icon'/>
+//               <p>HTML,CSS</p>
+//             </li>
+//             <li>
+//               <VscCheckAll className='service__list-icon'/>
+//               <p>HTML,CSS</p>
+//             </li>
+//           </ul>
+//         </article>
+//       </div> 
+      
+//     </section>
+//   )
+// }
+
+// export default Services
